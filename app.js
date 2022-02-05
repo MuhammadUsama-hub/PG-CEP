005cfd56d8076c9588cf567c9a023ae8d5c706d3
MuhammadUsama-hub <64538880+MuhammadUsama-hub@users.noreply.github.com>
2022-02-05 12:14:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,102 @@
+function calc1()
+{
+    var interest=document.getElementById('interest').value;
+    var capital=document.getElementById('capital').value;
+    var operational=document.getElementById('operational').value;
+    var unitsPerAnnum=1707800*(0.7957/100)*8760;
+    var totalCapCost=capital*unitsPerAnnum;
+    var totalOperCost=operational*unitsPerAnnum;
+    var fixedCost=(interest/100)*totalCapCost;
+    var totalAnulCost=fixedCost+totalOperCost;
+    var costPerUnit=totalAnulCost/unitsPerAnnum;
+    document.getElementById('Opportunity Cos').innerHTML=totalOperCost.toFixed(3);
+    document.getElementById('Capital Cost').innerHTML=totalCapCost.toFixed(3);
+    document.getElementById('Annual Cost').innerHTML=totalAnulCost.toFixed(3);
+    document.getElementById('Unit').innerHTML=costPerUnit;
+  
+}
+function calc2()
+{
+    var interest=document.getElementById('interest').value;
+    var capital=document.getElementById('capital').value;
+    var operational=document.getElementById('operational').value;
+    var unitsPerAnnum=2350000*(0.3537/100)*8760;
+    var totalCapCost=capital*unitsPerAnnum;
+    var totalOperCost=operational*unitsPerAnnum;
+    var fixedCost=(interest/100)*totalCapCost;
+    var totalAnulCost=fixedCost+totalOperCost;
+    var costPerUnit=totalAnulCost/unitsPerAnnum;
+    document.getElementById('Opportunity Cos').innerHTML=totalOperCost.toFixed(3);
+    document.getElementById('Capital Cost').innerHTML=totalCapCost.toFixed(3);
+    document.getElementById('Annual Cost').innerHTML=totalAnulCost.toFixed(3);
+    document.getElementById('Unit').innerHTML=costPerUnit;
+  
+}
+function calc3()
+{
+    var interest=document.getElementById('interest').value;
+    var capital=document.getElementById('capital').value;
+    var operational=document.getElementById('operational').value;
+    var unitsPerAnnum=648400*(0.5855/100)*8760;
+    var totalCapCost=capital*unitsPerAnnum;
+    var totalOperCost=operational*unitsPerAnnum;
+    var fixedCost=(interest/100)*totalCapCost;
+    var totalAnulCost=fixedCost+totalOperCost;
+    var costPerUnit=totalAnulCost/unitsPerAnnum;
+    document.getElementById('Opportunity Cos').innerHTML=totalOperCost.toFixed(3);
+    document.getElementById('Capital Cost').innerHTML=totalCapCost.toFixed(3);
+    document.getElementById('Annual Cost').innerHTML=totalAnulCost.toFixed(3);
+    document.getElementById('Unit').innerHTML=costPerUnit;
+  
+}
+function calc4()
+{
+    var interest=document.getElementById('interest').value;
+    var capital=document.getElementById('capital').value;
+    var operational=document.getElementById('operational').value;
+    var unitsPerAnnum=1772700*(0.5593/100)*8760;
+    var totalCapCost=capital*unitsPerAnnum;
+    var totalOperCost=operational*unitsPerAnnum;
+    var fixedCost=(interest/100)*totalCapCost;
+    var totalAnulCost=fixedCost+totalOperCost;
+    var costPerUnit=totalAnulCost/unitsPerAnnum;
+    document.getElementById('Opportunity Cos').innerHTML=totalOperCost.toFixed(3);
+    document.getElementById('Capital Cost').innerHTML=totalCapCost.toFixed(3);
+    document.getElementById('Annual Cost').innerHTML=totalAnulCost.toFixed(3);
+    document.getElementById('Unit').innerHTML=costPerUnit;
+  
+}
+function calc5()
+{
+    var interest=document.getElementById('interest').value;
+    var capital=document.getElementById('capital').value;
+    var operational=document.getElementById('operational').value;
+    var unitsPerAnnum=291710*(0.5623/100)*8760;
+    var totalCapCost=capital*unitsPerAnnum;
+    var totalOperCost=operational*unitsPerAnnum;
+    var fixedCost=(interest/100)*totalCapCost;
+    var totalAnulCost=fixedCost+totalOperCost;
+    var costPerUnit=totalAnulCost/unitsPerAnnum;
+    document.getElementById('Opportunity Cos').innerHTML=totalOperCost.toFixed(3);
+    document.getElementById('Capital Cost').innerHTML=totalCapCost.toFixed(3);
+    document.getElementById('Annual Cost').innerHTML=totalAnulCost.toFixed(3);
+    document.getElementById('Unit').innerHTML=costPerUnit;
+  
+}
+function calc6()
+{
+    var interest=document.getElementById('interest').value;
+    var capital=document.getElementById('capital').value;
+    var operational=document.getElementById('operational').value;
+    var unitsPerAnnum=10686000*(0.9045/100)*8760;
+    var totalCapCost=capital*unitsPerAnnum;
+    var totalOperCost=operational*unitsPerAnnum;
+    var fixedCost=(interest/100)*totalCapCost;
+    var totalAnulCost=fixedCost+totalOperCost;
+    var costPerUnit=totalAnulCost/unitsPerAnnum;
+    document.getElementById('Opportunity Cos').innerHTML=totalOperCost.toFixed(3);
+    document.getElementById('Capital Cost').innerHTML=totalCapCost.toFixed(3);
+    document.getElementById('Annual Cost').innerHTML=totalAnulCost.toFixed(3);
+    document.getElementById('Unit').innerHTML=costPerUnit;
+  
+}
